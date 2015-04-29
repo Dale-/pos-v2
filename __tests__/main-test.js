f7c724bac94ec29c90e5726bbabeb7d1260c2841
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-04-29 17:33:02 +0800

    Dale: finish same cartItem of "-" record with promotion

--- __tests__/main-test.js
+++ __tests__/main-test.js
@@ -80,6 +80,29 @@ describe('Pos Inventory', function() {
                 '********************** ';
             expect(expectResult).toBe(result);
         });
+
+        it('should return same cartItem of "-" record with promotion', function() {
+
+            var input = ['ITEM000001-3', 'ITEM000002'];
+            expectResult = PrintInventory(input);
+
+            var result = '***<没钱赚商店>购物清单***\n' +
+                '打印时间:' + moment().format('YYYY年-MM月-DD日 HH:mm:ss') + '\n' +
+                '----------------------\n' +
+                '名称：雪碧，数量：3瓶，单价：3.00(元)，小计：9.00(元)\n' +
+                '名称：苹果，数量：1斤，单价：5.50(元)，小计：5.50(元)\n' +
+                '----------------------\n' +
+                '挥泪赠送商品：\n' +
+                '名称：雪碧，数量：1瓶\n' +
+                '----------------------\n' +
+                '总计：11.50(元)\n' +
+                '节省：3.00(元)\n' +
+                '********************** ';
+            expect(expectResult).toBe(result);
+        });
+
+
+
     });
 
 
