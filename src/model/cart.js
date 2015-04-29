d7a4ed6cd2a67c74ab7abeadd749f85c949f607e
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-04-29 14:21:50 +0800

    Dale: finish transfer input to cartitems

--- src/model/cart.js
+++ src/model/cart.js
@@ -1,7 +1,29 @@
 var moment = require('moment');
+var _ = require('lodash');
+var Item = require('./item.js');
+var CartItem = require('./cartItem.js');
 
 var Cart = function () {
+    this.cartItems = [];
+};
 
+Cart.prototype.getCartItemsInformation = function() {
+
+};
+
+Cart.prototype.toCartItems = function(tags) {
+
+    var _this = this;
+
+    _.forEach(tags, function(tag) {
+
+        var item = _.find(Item.loadAllItems(), function(item) {
+           return item.barcode === tag;
+        });
+
+        _this.cartItems.push(new CartItem(item, 1));
+    });
+    return this.cartItems;
 };
 
 Cart.prototype.toInventory = function() {
@@ -9,12 +31,11 @@ Cart.prototype.toInventory = function() {
     var list =  '***<没钱赚商店>购物清单***' +
                 '打印时间:' + moment().format('YYYY年-MM月-DD日 HH:mm:ss') +
                 '----------------------' +
-                '名称：雪碧，数量：1个，单价：3.00(元)，小计：3.00(元)' +
+                '名称：' + '，数量：' + '，单价：' + '(元)，小计：' + '(元)' +
                 '----------------------' +
-                '总计：3.00(元)' +
+                '总计：' + '(元)' +
                 '********************** ';
 
-    console.log(list);
     return list;
 };
 
